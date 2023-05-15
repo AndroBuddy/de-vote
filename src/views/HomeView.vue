aa829771df092c8c9745c1ed70b007ec89df4778
AndroBuddy <anirudhchimpidi8@gmail.com>
2023-05-15 15:26:06 +0530

    Content: make those scrolling edges fade in
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,15 @@ const testItems = [
   { name: 'Product 1', id: '1', img: '', price: '$10', quickBid: '$2' },
   { name: 'Product 2', id: '2', img: '', price: '$20', quickBid: '$5' },
   { name: 'Product 3', id: '3', img: '', price: '$13', quickBid: '$4' },
-  { name: 'Product 4', id: '4', img: '', price: '$15', quickBid: '$1' }
+  { name: 'Product 4', id: '4', img: '', price: '$15', quickBid: '$1' },
+  { name: 'Product 5', id: '4', img: '', price: '$15', quickBid: '$1' },
+  { name: 'Product 6', id: '4', img: '', price: '$15', quickBid: '$1' }
 ]
 </script>
 
 <template>
-  <section class="flex flex-col px-4 py-20 lg:px-14 flex-grow gap-12 xl:self-center">
-    <section class="flex flex-col gap-10">
+  <section class="flex flex-col px-4 py-20 lg:px-14 flex-grow gap-12">
+    <section class="flex flex-col gap-10 xl:self-center container">
       <ContentPanel title="Recent Auctions" :items="testItems" />
       <ContentPanel title=" High Value Bids" :items="testItems" />
     </section>
