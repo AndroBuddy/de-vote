0aa1151818ba679e609c79e4ffa172adfd2cc441
Srikar <t1srikar@gmail.com>
2023-12-14 16:30:31 +0530

    Changes in forms

--- src/views/BuildBallotView.vue
+++ src/views/BuildBallotView.vue
@@ -24,6 +24,10 @@ function toggleChangePollTitle() {
   changePollTitle.value = !changePollTitle.value
 }
 
+function submitOption() {
+  
+}
+
 function onEnter(el, done) {
   anime({
     targets: el,
@@ -51,21 +55,35 @@ function onLeave(el, done) {
   <!-- Modal -->
   <TransitionRoot as="template" :show="optionModal">
     <Dialog as="div" class="relative z-40" @close="toggleModal">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
-        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
         <div class="fixed inset-0 bg-black/25" />
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300"
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
             enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
             leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
             <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white text-left transition-all sm:w-[36rem]">
-              <section class="flex flex-col gap-10 p-10 rounded-3xl w-full max-h-[90vh] overflow-scroll">
+              class="relative transform overflow-hidden rounded-lg bg-white text-left transition-all sm:w-[36rem]"
+            >
+              <section
+                class="flex flex-col gap-10 p-10 rounded-3xl w-full max-h-[90vh] overflow-scroll"
+              >
                 <DialogTitle class="flex flex-col gap-2">
                   <h1 class="text-2xl">Add Option</h1>
                 </DialogTitle>
@@ -73,14 +91,74 @@ function onLeave(el, done) {
                 <section class="flex flex-col justify-center">
                   <form class="flex flex-col gap-4" @submit.prevent="submit">
                     <div class="flex flex-col gap-2">
-                      <label for="title" class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
-                        Title
+                      <label
+                        for="nominee"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Nominee Name
                       </label>
-                      <input id="title" name="title" required="true"
-                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-900 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
+                      <input
+                        id="nominee"
+                        name="nominee"
+                        type="text"
+                        placeholder="Nominee"
+                        required="true"
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
                     </div>
-                    <button type="submit"
-                      class="self-start flex justify-center items-center rounded-md bg-blue-800 px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none mt-4">
+                    <div class="flex flex-col gap-2">
+                      <label
+                        for="region"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Region
+                      </label>
+                      <input
+                        id="region"
+                        name="region"
+                        type="text"
+                        placeholder="Region"
+                        required="true"
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
+                    </div>
+                    <div class="flex flex-col gap-2">
+                      <label
+                        for="location"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Location
+                      </label>
+                      <input
+                        id="location"
+                        name="location"
+                        type="text"
+                        placeholder="Location"
+                        required="true"
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
+                    </div>
+                    <div class="flex flex-col gap-2">
+                      <label
+                        for="party"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Party
+                      </label>
+                      <input
+                        id="party"
+                        name="party"
+                        type="text"
+                        placeholder="Party"
+                        required="true"
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      @onclick="submitOption"
+                      class="self-start flex justify-center items-center rounded-md bg-blue-800 px-4 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none mt-4"
+                    >
                       Add
                     </button>
                   </form>
@@ -101,12 +179,17 @@ function onLeave(el, done) {
   <section class="flex flex-col sm:items-center pt-20 w-full h-full">
     <Transition @enter="onEnter" @leave="onLeave" mode="out-in">
       <section v-if="buildBallot">
-        <div class="flex flex-col gap-3 items-center justify-center border-black/20 border p-10 rounded-lg sm:w-96">
+        <div
+          class="flex flex-col gap-3 items-center justify-center border-black/20 border p-10 rounded-lg sm:w-96"
+        >
           <DirectboxDefault size="40" />
           <h5>Build your first poll</h5>
           <section class="flex flex-col justify-center">
-            <button type="submit" @click="toggleBuildBallot"
-              class="flex w-full justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none">
+            <button
+              type="submit"
+              @click="toggleBuildBallot"
+              class="flex w-full justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none"
+            >
               Build Ballot
             </button>
           </section>
@@ -121,22 +204,30 @@ function onLeave(el, done) {
                 <input />
               </form>
               <button @onclick="toggleChangePollTitle" v-else>
-                <span class="font-semibold leading-6 text-gray-900 overflow-clip text-ellipsis" id="poll-title">
-                  Election
+                <span
+                  class="font-semibold leading-6 text-gray-900 overflow-clip text-ellipsis"
+                  id="poll-title"
+                >
+                  Election Ballot
                 </span>
               </button>
               <button @click="toggleBuildBallot" class="">
                 <Trash style="color: red" />
               </button>
             </div>
-            <p class="mt-1 truncate text-sm leading-5 text-gray-500">Description</p>
+            <p class="mt-1 truncate text-sm leading-5 text-gray-500">
+              Add nominees to your election ballot
+            </p>
           </div>
           <section class="flex gap-4 items-center justify-center pb-4">
             <div class="border flex-grow" />
           </section>
-          <button type="submit" @click="toggleModal"
-            class="flex self-start justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none">
-            Add Option
+          <button
+            type="submit"
+            @click="toggleModal"
+            class="flex self-start justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none"
+          >
+            Add Nominee
           </button>
         </div>
       </section>
--- src/views/VotersListView.vue
+++ src/views/VotersListView.vue
@@ -43,21 +43,35 @@ function onLeave(el, done) {
   <!-- Modal -->
   <TransitionRoot as="template" :show="optionModal">
     <Dialog as="div" class="relative z-40" @close="toggleModal">
-      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
-        leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
         <div class="fixed inset-0 bg-black/25" />
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
-          <TransitionChild as="template" enter="ease-out duration-300"
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
             enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
             leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
             <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white text-left transition-all sm:w-[36rem]">
-              <section class="flex flex-col gap-5 p-10 rounded-3xl w-full max-h-[90vh] overflow-scroll">
+              class="relative transform overflow-hidden rounded-lg bg-white text-left transition-all sm:w-[36rem]"
+            >
+              <section
+                class="flex flex-col gap-5 p-10 rounded-3xl w-full max-h-[90vh] overflow-scroll"
+              >
                 <DialogTitle>
                   <h1 class="text-2xl">Add Voter</h1>
                 </DialogTitle>
@@ -65,54 +79,98 @@ function onLeave(el, done) {
                 <section class="flex flex-col justify-center">
                   <form class="flex flex-col gap-4" @submit.prevent="submit">
                     <div class="flex flex-col gap-2">
-                      <label for="email" class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
-                        Email Address
-                      </label>
-                      <input id="email" name="email" type="email" autocomplete="email" placeholder="Email Address"
-                        required="true"
-                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
-                    </div>
-                    <div class="flex flex-col gap-2">
                       <div class="flex flex-row gap-5">
                         <div class="flex flex-col gap-2">
-                          <label for="first-name"
-                            class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
+                          <label
+                            for="first-name"
+                            class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                          >
                             First Name
                           </label>
-                          <input id="first-name" name="first-name" type="text" autocomplete="fname"
-                            placeholder="First Name" required="true"
-                            class="block rounded-md border border-slate-300 w-full py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
+                          <input
+                            id="first-name"
+                            name="first-name"
+                            type="text"
+                            autocomplete="fname"
+                            placeholder="First Name"
+                            required="true"
+                            class="block rounded-md border border-slate-300 w-full py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                          />
                         </div>
                         <div class="flex flex-col gap-2">
-                          <label for="last-name"
-                            class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
+                          <label
+                            for="last-name"
+                            class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                          >
                             Last Name
                           </label>
-                          <input id="last-name" name="last-name" type="text" autocomplete="lname" placeholder="Last Name"
+                          <input
+                            id="last-name"
+                            name="last-name"
+                            type="text"
+                            autocomplete="lname"
+                            placeholder="Last Name"
                             required="true"
-                            class="block rounded-md border border-slate-300 w-full py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
+                            class="block rounded-md border border-slate-300 w-full py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                          />
                         </div>
                       </div>
                     </div>
                     <div class="flex flex-col gap-2">
-                      <label for="wallet-id"
-                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
-                        Wallet ID
+                      <label
+                        for="age"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Age
                       </label>
-                      <input id="wallet-id" name="wallet-id" type="text" autocomplete="walletID" placeholder="Wallet ID"
+                      <input
+                        id="age"
+                        name="age"
+                        type="number"
+                        autocomplete="age"
+                        placeholder="Age"
                         required="true"
-                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500 focus:ring-1 focus:ring-sky-500 sm:leading-6"
+                      />
                     </div>
                     <div class="flex flex-col gap-2">
-                      <label for="email" class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']">
-                        Passkey
+                      <label
+                        for="region"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Region
                       </label>
-                      <input id="email" name="email" type="email" autocomplete="email" placeholder="Passkey"
+                      <input
+                        id="region"
+                        name="region"
+                        type="text"
+                        autocomplete="region"
+                        placeholder="Region"
                         required="true"
-                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6" />
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
                     </div>
-                    <button type="submit"
-                      class="self-start flex justify-center items-center rounded-md bg-blue-800 px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none mt-4">
+                    <div class="flex flex-col gap-2">
+                      <label
+                        for="voter-id"
+                        class="block text-sm font-medium leading-6 text-gray-900 after:content-['*']"
+                      >
+                        Voter ID (Aadhar)
+                      </label>
+                      <input
+                        id="voter-id"
+                        name="voter-id"
+                        type="text"
+                        autocomplete="voterID"
+                        placeholder="Voter ID"
+                        required="true"
+                        class="block rounded-md border border-slate-300 py-1.5 px-2 placeholder:text-gray-400 placeholder:text-xs focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 sm:leading-6 invalid:border-red-200 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      class="self-start flex justify-center items-center rounded-md bg-blue-800 px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none mt-4"
+                    >
                       Add Voter
                     </button>
                   </form>
@@ -130,8 +188,11 @@ function onLeave(el, done) {
     <div class="text-lg font-bold">Voters</div>
     <Transition @enter="onEnter" @leave="onLeave" mode="out-in">
       <section v-if="!buildBallot">
-        <button type="submit" @click="toggleModal"
-          class="flex self-start justify-center items-center rounded-md bg-blue-100 px-3 py-1.5 text-sm leading-6 text-blue-800 shadow-sm hover:bg-blue-200 focus-visible:outline-none">
+        <button
+          type="submit"
+          @click="toggleModal"
+          class="flex self-start justify-center items-center rounded-md bg-blue-100 px-3 py-1.5 text-sm leading-6 text-blue-800 shadow-sm hover:bg-blue-200 focus-visible:outline-none"
+        >
           Add Voter
         </button>
       </section>
@@ -141,12 +202,17 @@ function onLeave(el, done) {
   <section class="flex flex-col sm:items-center pt-20 w-full h-full">
     <Transition @enter="onEnter" @leave="onLeave" mode="out-in">
       <section v-if="buildBallot">
-        <div class="flex flex-col gap-3 items-center justify-center border-black/20 border p-10 rounded-lg sm:w-96">
+        <div
+          class="flex flex-col gap-3 items-center justify-center border-black/20 border p-10 rounded-lg sm:w-96"
+        >
           <People size="40" />
           <h5>Add eligible voters</h5>
           <section class="flex flex-col justify-center">
-            <button type="submit" @click="toggleBuildBallot"
-              class="flex w-full justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none">
+            <button
+              type="submit"
+              @click="toggleBuildBallot"
+              class="flex w-full justify-center items-center rounded-md bg-blue-800 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline-none"
+            >
               Add Voter
             </button>
           </section>
@@ -155,7 +221,9 @@ function onLeave(el, done) {
 
       <section v-else key="2">
         <div class="border border-black/20 relative overflow-x-auto sm:rounded-lg">
-          <table class="w-full table-auto text-left rtl:text-right text-gray-600 dark:text-gray-400">
+          <table
+            class="w-full table-auto text-left rtl:text-right text-gray-600 dark:text-gray-400"
+          >
             <thead class="text-black font-semibold bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" class="px-6 py-3">Email Address</th>
@@ -166,7 +234,9 @@ function onLeave(el, done) {
               </tr>
             </thead>
             <tbody>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <tr
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
                 <td class="px-6 py-4">example@mahindrauniversity.edu.in</td>
                 <td class="px-6 py-4">0x12345678</td>
                 <td class="px-6 py-4">DeVote</td>
